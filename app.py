5396c8efdd182833ee76b78144ae35a9bcaf438f
Mazzoldi <128252460+Mazzoldi@users.noreply.github.com>
2025-01-21 19:29:29 +0100

    Initial commit

---
+++ app.py
@@ -0,0 +1,42 @@
+from flask import Flask, render_template, jsonify, request
+import requests
+
+app = Flask(__name__)
+
+API_BLOCKS_URL = "https://blockstream.info/api/blocks/"
+API_MEMPOOL_URL = "https://blockstream.info/api/mempool/recent"
+
+@app.route("/")
+def homepage():
+    return render_template("index.html")
+
+@app.route("/blocks")
+def blocks_page():
+    return render_template("blocks.html")
+
+@app.route("/api/blocks", methods=["GET"])
+def get_blocks():
+    last_block_height = request.args.get("lastBlockHeight")
+    url = f"{API_BLOCKS_URL}{last_block_height}" if last_block_height else API_BLOCKS_URL
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        return jsonify(response.json())
+    except requests.RequestException as e:
+        return jsonify({"error": str(e)}), 500
+
+@app.route("/mempool")
+def mempool_page():
+    return render_template("mempool.html")
+
+@app.route("/api/mempool", methods=["GET"])
+def get_mempool_transactions():
+    try:
+        response = requests.get(API_MEMPOOL_URL)
+        response.raise_for_status()
+        return jsonify(response.json())
+    except requests.RequestException as e:
+        return jsonify({"error": str(e)}), 500
+
+if __name__ == "__main__":
+    app.run(debug=False)
