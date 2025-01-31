ffac0826867d0e822d920994968843f26d5a91f8
Mazzoldi <128252460+Mazzoldi@users.noreply.github.com>
2025-01-31 12:56:59 +0100

    Added features

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import requests
 app = Flask(__name__)
 
 API_BLOCKS_URL = "https://blockstream.info/api/blocks/"
+API_BLOCK_DETAILS_URL = "https://blockstream.info/api/block/"
 API_MEMPOOL_URL = "https://blockstream.info/api/mempool/recent"
 
 @app.route("/")
@@ -13,6 +14,13 @@ def homepage():
 @app.route("/blocks")
 def blocks_page():
     return render_template("blocks.html")
+
+@app.route('/block-details', methods=['GET'])
+def block_details_page():
+    block_id = request.args.get('id')
+    if not block_id:
+        return "Errore: ID del blocco non fornito", 400
+    return render_template('block-details.html', block_id=block_id)
 
 @app.route("/api/blocks", methods=["GET"])
 def get_blocks():
@@ -28,6 +36,13 @@ def get_blocks():
 @app.route("/mempool")
 def mempool_page():
     return render_template("mempool.html")
+
+@app.route("/transaction-details")
+def transaction_details_page():
+    tx_id = request.args.get("id")
+    if not tx_id:
+        return "Errore: ID della transazione non fornito", 400
+    return render_template("transaction-details.html", tx_id=tx_id)
 
 @app.route("/api/mempool", methods=["GET"])
 def get_mempool_transactions():
